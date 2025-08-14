17e8cacd300d6dda5580b8e24ac517ed33fcd3a0
Madee <130764617+MadeBayuSW@users.noreply.github.com>
2025-08-14 22:26:59 +0700

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ st.set_page_config(
 
 @st.cache_data
 def load_data():
-    return pd.read_csv("data/online_retail_store.csv")
+    return pd.read_csv("Data/online_retail_store.csv")
 
 # Load data penjualan
 df_sales = load_data()
